ae5130a44da0b615e83ac34fc9d1c39a33e49455
jacob24 <abello.jacob@gmail.com>
2018-01-09 10:40:19 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow "
+                      "the instructions for installing setuptools")
+
+setup(
+    name='objmatcher',
+    url='https://github.com/jacobabello/objmatcher',
+    version='0.01',
+    author='Jacob Abello',
+    author_email='abello.jacob@gmail.com',
+    description='Match Generated Object and Return Similarity Score',
+    packages=[
+        'objmatcher'
+    ],
+    requires=[
+        'numpy'
+    ]
+)
