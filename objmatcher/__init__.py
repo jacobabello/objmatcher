bc26570716330e36d99900c4c0d9f39dd69122fa
jacob24 <abello.jacob@gmail.com>
2018-01-11 14:11:49 +0800

    modify to handle list within key

--- objmatcher/__init__.py
+++ objmatcher/__init__.py
@@ -6,37 +6,40 @@ import numpy as np
 
 def match(obj1, obj2):
     word_lists = OrderedDict()
-    similarity_average = []
+    similarity_average_list = []
 
     for key, value in obj1.iteritems():
-        word_lists.update({key: str.split(value)})
+        word_lists.update({key: [str.split(x) for x in value]})
 
     for key, value in obj2.iteritems():
-        word_lists.update({key: word_lists[key] + list(set(str.split(value)) - set(word_lists[key]))})
+        for index in range(len(word_lists[key])):
+            for x in range(len(value)):
+                word_lists[key][index] += list(set(str.split(value[x])) - set(word_lists[key][index]))
 
-    print(word_lists)
+    for key in word_lists:
+        for x in range(len(word_lists[key])):
+            vector1 = list(np.zeros(len(word_lists[key][x]), dtype=np.int))
+            vector2 = list(np.zeros(len(word_lists[key][x]), dtype=np.int))
 
-    for _ in word_lists:
-        vector1 = list(np.zeros(len(word_lists[_]), dtype=np.int))
-        vector2 = list(np.zeros(len(word_lists[_]), dtype=np.int))
+            print(word_lists[key][x])
+            print(obj1[key][x])
+            print(obj2[key][0])
 
-        print(word_lists[_])
-        print(str.split(obj1[_]))
-        print(str.split(obj2[_]))
+            for index in range(len(word_lists[key][x])):
+                if word_lists[key][x][index] in str.split(obj1[key][x]):
+                    vector1[index] = 1
+                if word_lists[key][x][index] in str.split(obj2[key][0]):
+                    if vector1[index] == 1:
+                        vector1[index] = 2
+                        vector2[index] = 2
+                    else:
+                        vector2[index] = 1
 
-        for x in range(len(word_lists[_])):
-            if word_lists[_][x] in str.split(obj1[_]):
-                vector1[x] = 1
-            if word_lists[_][x] in str.split(obj2[_]):
-                if vector1[x] == 1:
-                    vector1[x] = 2
-                    vector2[x] = 2
-                else:
-                    vector2[x] = 1
 
-        similarity_average.append(get_similarity(vector1, vector2))
-
-    return round(np.mean(similarity_average), 3)
+            print(vector1)
+            print(vector2)
+            print(get_similarity(vector1, vector2))
+            print('---')
 
 
 def get_similarity(x, y):
--- test/test_matcher_with_json_data.py
+++ test/test_matcher_with_json_data.py
@@ -5,13 +5,42 @@ from objmatcher import match
 class TestMatcherWithJsonData(unittest.TestCase):
     def test_two_json_with_high_similarity(self):
         data1 = {
-            'Name': 'MSI COMPUTER CORP USA',
-            # 'Address': '601 NORTH WALTON BLVD BENTONVILLE AR 72716 USA'
+            'Name': [
+                'SAMS CLUB WALMART STORES INC',
+                'WALMART STORES INC WALMART STORE',
+                'WALMART STORES S A'
+            ],
+            'Address': [
+                '601 NORTH WALTON BLVD BENTONVILLE AR 72716 USA'
+            ]
         }
 
         data2 = {
-            'Name': 'MSI COMPUTER CORP',
-            # 'Address': '601 N WALTON BLVD BENTONVILLE AR 72716 USA'
+            'Name': [
+                'WALMART STORES INC XXX'
+            ],
+            'Address': [
+                '601 NORTH WALTON BLVD BENTONVILLE AR 72712 USA'
+            ]
         }
 
         self.assertGreaterEqual(match(data1, data2), 0.9)
+
+    # def test_json_with_list_of_data(self):
+    #     data1 = {
+    #         'Name': ['HEWLETT PACKARD ENTERPRISE COMPANY'],
+    #         'Address': [
+    #             '3000 HANOVER STREET PALO ALTO CA 94304 USA',
+    #             '6701 LEGACY BLVD, OLIVE BRANCH, MS 38654, USA',
+    #             '11445 COMPAQ CENTER DR HOUSTON TX 77070 USA',
+    #             '4501 BLALOCK, HOUSTON, TX 77041, USA'
+    #         ]
+    #     }
+    #
+    #     data2 = {
+    #         'Name': ['HPE'],
+    #         'Address': ['11445 COMPAQ CENTER DR HOUSTON TX 77070 USA']
+    #     }
+    #
+    #     print(match(data1, data2))
+    #     assert False
