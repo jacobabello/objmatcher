5e6bd0f4189719b4e13bd4f512acd6be727d5743
jacob24 <abello.jacob@gmail.com>
2018-04-18 14:54:08 +0800

    add test for completely different data

--- test/test_matcher.py
+++ test/test_matcher.py
@@ -25,3 +25,54 @@ class test_matcher(unittest.TestCase):
         matcher.match()
 
         self.assertGreaterEqual(matcher.get_average_scores(), 0.9)
+
+    def test_matching_data1_has_more_key(self):
+        data1 = self.object_matcher.generate_object()
+        data1.add_meta_data('name', 'red hot chili peppers')
+        data1.add_meta_data('song', 'can\'t stop')
+        data1.add_meta_data('song', 'hey oh!')
+
+        data2 = self.object_matcher.generate_object()
+        data2.add_meta_data('name', 'red hot chili peppers')
+        data2.add_meta_data('song', 'can\'t stop')
+        data2.add_meta_data('song', 'snow hey oh!')
+        data2.add_meta_data('song', 'californication')
+        data2.add_meta_data('vocalist', 'Anthony Kiedis')
+
+        matcher = Matcher(data1, data2)
+        matcher.match()
+
+        self.assertGreaterEqual(matcher.get_average_scores(), 0.9)
+
+    def test_handling_blank_value(self):
+        data1 = self.object_matcher.generate_object()
+        data1.add_meta_data('name', 'red hot chili peppers')
+        data1.add_meta_data('song', 'can\'t stop')
+        data1.add_meta_data('song', 'hey oh!')
+        data1.add_meta_data('vocalist', '')
+
+        data2 = self.object_matcher.generate_object()
+        data2.add_meta_data('name', 'red hot chili peppers')
+        data2.add_meta_data('song', 'can\'t stop')
+        data2.add_meta_data('song', 'snow hey oh!')
+        data2.add_meta_data('song', 'californication')
+
+        matcher = Matcher(data1, data2)
+        matcher.match()
+
+        self.assertGreaterEqual(matcher.get_average_scores(), 0.9)
+
+    def test_completely_different_data(self):
+        data1 = self.object_matcher.generate_object()
+        data1.add_meta_data('name', 'foo fighters')
+        data1.add_meta_data('song', 'best of you')
+
+        data2 = self.object_matcher.generate_object()
+        data2.add_meta_data('name', 'nirvana')
+        data2.add_meta_data('song', 'smell like teen spirit')
+        data2.add_meta_data('genre', 'grundge')
+
+        matcher = Matcher(data1, data2)
+        matcher.match()
+
+        self.assertGreaterEqual(matcher.get_average_scores(), 0.0)
